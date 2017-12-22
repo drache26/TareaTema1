1c87ba7a977f84f6e1e49ff8e316da10b1854915
danieldinardo <dadiname55@hotmail.com>
2017-12-22 14:09:14 +0100

    Commit inicial

---
+++ src/com/cip/prog/TareaTema1.java
@@ -0,0 +1,20 @@
+package com.cip.prog;
+
+public class TareaTema1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bienvenido al sistema");
+		
+		System.out.println("Hoy es lunes");
+		
+		System.out.println("Son las 08:00");
+		
+		System.out.println("Tienes 8 correos sin leer");
+		
+		
+
+	}
+
+}
